169548a98d81cc6b01920c0fc224ddce7a0bc0c2
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-19 09:51:28 +0800

    chore: remove test logging

--- src/index.js
+++ src/index.js
@@ -231,10 +231,7 @@ export async function run() {
       if (tmateWeb) {
         core.info(`Web shell: ${tmateWeb}`);
       }
-      core.info(`SSH: ssh -p ${port} ${token}@${host} (info)`);
-      core.debug(`SSH: ssh -p ${port} ${token}@${host} (debug)`);
-      core.warning(`SSH: ssh -p ${port} ${token}@${host} (warning)`);
-      execShellCommand(`echo "SSH: ssh -p ${port} ${token}@${host} (echo)"`)
+      core.info(`SSH: ssh -p ${port} ${token}@${host}`);
       if (tmateSSHDashI) {
         core.info(`or: ${tmateSSH.replace(/^ssh/, tmateSSHDashI)}`)
       }
