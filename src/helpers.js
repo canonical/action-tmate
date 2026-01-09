81cb08934ab7a8ea542be1408ebc7c9985af352c
Weii Wang <weii.wang@canonical.com>
2026-01-09 15:25:58 +0800

    Use default value of ssh and keep the order

--- src/helpers.js
+++ src/helpers.js
@@ -104,29 +104,29 @@ export const updateSshConfig = async (host) => {
     const cipherList = (await execShellCommand("ssh -Q cipher", {quiet: true})).trim().split("\n")
     const ciphers = cipherAllowList.filter(c => cipherList.includes(c)).join(",")
     const keyAlgoAllowList = [
-      "ssh-ed25519-cert-v01@openssh.com",
+      // "ssh-ed25519-cert-v01@openssh.com",
       "ecdsa-sha2-nistp256-cert-v01@openssh.com",
       "ecdsa-sha2-nistp384-cert-v01@openssh.com",
       "ecdsa-sha2-nistp521-cert-v01@openssh.com",
-      "sk-ssh-ed25519-cert-v01@openssh.com",
-      "sk-ecdsa-sha2-nistp256-cert-v01@openssh.com",
+      // "sk-ssh-ed25519-cert-v01@openssh.com",
+      // "sk-ecdsa-sha2-nistp256-cert-v01@openssh.com",
       "rsa-sha2-512-cert-v01@openssh.com",
       "rsa-sha2-256-cert-v01@openssh.com",
-      "ssh-ed25519",
+      // "ssh-ed25519",
       "ecdsa-sha2-nistp256",
       "ecdsa-sha2-nistp384",
       "ecdsa-sha2-nistp521",
-      "sk-ecdsa-sha2-nistp256@openssh.com",
-      "sk-ssh-ed25519@openssh.com",
+      // "sk-ecdsa-sha2-nistp256@openssh.com",
+      // "sk-ssh-ed25519@openssh.com",
       "rsa-sha2-512",
       "rsa-sha2-256"
     ]
     const keyAlgoList = (await execShellCommand("ssh -Q key", {quiet: true})).trim().split("\n")
     const keyAlgos = keyAlgoAllowList.filter(k => keyAlgoList.includes(k)).join(",")
     const kexAlgoAllowList = [
-      "sntrup761x25519-sha512@openssh.com",
-      "curve25519-sha256",
-      "curve25519-sha256@libssh.org",
+      // "sntrup761x25519-sha512@openssh.com",
+      // "curve25519-sha256",
+      // "curve25519-sha256@libssh.org",
       "ecdh-sha2-nistp256",
       "ecdh-sha2-nistp384",
       "ecdh-sha2-nistp521",
